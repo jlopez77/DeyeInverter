bcb0c26a541befc23727d254d3496947a4493144
jlopez77 <jlopez77@users.noreply.github.com>
2021-07-15 16:29:27 +0200

    Add files via upload
    Added mqtt support (basic)
    Removed some bugs
    Increased timeout
    More checks on received data

--- InverterData.py
+++ InverterData.py
@@ -1,9 +1,10 @@
+import sys
 import socket
 import binascii
 import re
 import libscrc
 import json
-from pprint import pprint
+import paho.mqtt.client as paho
 
 def twosComplement_hex(hexval):
     bits = 16
@@ -17,16 +18,29 @@ def twosComplement_hex(hexval):
 inverter_ip="192.168.X.XXX"
 inverter_port=8899
 inverter_sn=17XXXXXXXX
+mqtt=1
+mqtt_server="192.168.X.XXX"
+mqtt_port=1883
+mqtt_topic="XXXXXXXX"
 
 # END CONFIG
 
-# PREPARE DATA
+# Initialise MQTT if configured
 
+if mqtt==1: 
+ client=paho.Client("inverter")
+ client.connect(mqtt_server, mqtt_port)
+
+# PREPARE & SEND DATA TO THE INVERTER
+output="[{" # initialise json output
 pini=0
 chunks=0
 while chunks<2:
  pfin=pini+100
-
+ if chunks==-1: # testing initialisation
+  pini=235
+  pfin=235
+  print("Initialise Connection")
  start = binascii.unhexlify('A5') #start
  length=binascii.unhexlify('1700') # datalength
  controlcode= binascii.unhexlify('1045') #controlCode
@@ -55,13 +69,14 @@ while chunks<2:
                   family, socktype, proto, canonname, sockadress = res
                   try:
                    clientSocket= socket.socket(family,socktype,proto);
-                   clientSocket.settimeout(5);
+                   clientSocket.settimeout(10);
                    clientSocket.connect(sockadress);
                   except socket.error as msg:
                    print("Could not open socket");
                    break
  
  # SEND DATA
+ #print(chunks)
  clientSocket.sendall(frame_bytes);
  
  ok=False;
@@ -69,13 +84,16 @@ while chunks<2:
   try:
    data = clientSocket.recv(1024);
    ok=True
+   try:
+    data
+   except:
+    print("No data - Die")
+    sys.exit(1) #die, no data
   except socket.timeout as msg:
-   print("Comnnection timeout");
-   ok= True
-   continue
+   print("Connection timeout");
+   sys.exit(1) #die
  
  # PARSE RESPONSE (start position 56, end position 60)
- 
  
  i=pfin-pini
  a=0
@@ -92,9 +110,15 @@ while chunks<2:
      ratio=item["ratio"]
      unit=item["unit"]
      for register in item["registers"]:
-      if register==hexpos:
-       print(title+":"+str(response*ratio)+unit)
+      if register==hexpos and chunks!=-1:
+       #print(title+":"+str(response*ratio)+unit)
+       output=output+"\""+ title + "(" + unit + ")" + "\":" + str(response*ratio)+","
   a+=1
-
  pini+=100
  chunks+=1  
+output=output[:-1]+"}]"
+if mqtt==1:
+ client.publish(mqtt_topic,output)
+ print("Ok")
+else:
+ print(output)
